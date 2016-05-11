015d4fc14ced1eb5de031f7a617203027e6c7714
Jesse Bartola <jrbartola@gmail.com>
2016-05-11 15:23:07 -0400

    Altered scopium carousel, removed gradient

--- static/js/agency.js
+++ static/js/agency.js
@@ -16,6 +16,26 @@ $(document).ready(function() {
 	    });
 	});
 
+    // // cache the window object
+    // $window = $(window);
+ 
+    // $('[data-type="background"]').each(function(){
+    //   // declare the variable to affect the defined data-type
+    //   var $scroll = $(this);
+                     
+    //    $(window).scroll(function() {
+    //      // HTML5 proves useful for helping with creating JS functions!
+    //      // also, negative value because we're scrolling upwards                             
+    //      var yPos = -($window.scrollTop() / $scroll.data('speed')); 
+         
+    //      // background position
+    //      var coords = '50% '+ yPos + 'px';
+ 
+    //      // move the background
+    //      $scroll.css({ backgroundPosition: coords });    
+    //    }); // end window scroll
+    // });  // end section function
+
 	// Highlight the top nav as scrolling occurs
 	$('body').scrollspy({
 	    target: '.navbar-fixed-top'
