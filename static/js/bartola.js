a78ac9201e3ac60c50976a100a29d9ba23bbf72d
Jesse Bartola <jrbartola@gmail.com>
2016-05-11 15:37:25 -0400

    Fixed mailbox footer layout, changed main js name

--- static/js/bartola.js
+++ static/js/bartola.js
@@ -1,4 +1,4 @@
-/* http://www.jessebartola.com
+/* http://www.jessebartola.com/
  * 
  * 
  * Copyright Jesse Bartola 2016
@@ -15,26 +15,6 @@ $(document).ready(function() {
 	        event.preventDefault();
 	    });
 	});
-
-    // // cache the window object
-    // $window = $(window);
- 
-    // $('[data-type="background"]').each(function(){
-    //   // declare the variable to affect the defined data-type
-    //   var $scroll = $(this);
-                     
-    //    $(window).scroll(function() {
-    //      // HTML5 proves useful for helping with creating JS functions!
-    //      // also, negative value because we're scrolling upwards                             
-    //      var yPos = -($window.scrollTop() / $scroll.data('speed')); 
-         
-    //      // background position
-    //      var coords = '50% '+ yPos + 'px';
- 
-    //      // move the background
-    //      $scroll.css({ backgroundPosition: coords });    
-    //    }); // end window scroll
-    // });  // end section function
 
 	// Highlight the top nav as scrolling occurs
 	$('body').scrollspy({
