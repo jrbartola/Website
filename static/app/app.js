203da42af429ecd1f83650ec9ee9ea9371237e50
Jesse Bartola <jrbartola@gmail.com>
2016-12-29 13:24:16 -0500

    Angular app.js

---
+++ static/app/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var app = angular.module('website', []);
+
+app.config(['$interpolateProvider', function($interpolateProvider) {
+  $interpolateProvider.startSymbol('{a');
+  $interpolateProvider.endSymbol('a}');
+}]);
