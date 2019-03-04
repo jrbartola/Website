b08027ef13bf672d69de44797270f7a04f749b98
Jesse Bartola <jrbartola@gmail.com>
2019-03-03 23:22:41 -0500

    Refactor into react project structure

---
+++ www/react/container.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+import { Navbar } from './navbar';
+
+interface ContainerProps {}
+interface ContainerState {}
+
+/**
+ * Defines the container object for our React application
+ */
+export class Container extends React.Component<ContainerProps, ContainerState> {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div id="container">
+			    <Navbar />
+			</div>
+		)
+	}
+}
