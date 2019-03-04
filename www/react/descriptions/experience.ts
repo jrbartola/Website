ff7f4a2ac9a2d6f71694ceeaea1a9c2767f8635e
Jesse Bartola <jrbartola@gmail.com>
2019-03-04 14:05:30 -0500

    More resume implementation; finished experience and school sections

---
+++ www/react/descriptions/experience.ts
@@ -0,0 +1,30 @@
+/**
+ * File containing static text data that is used in the description of timeline fields in the resume accordion
+ */
+
+export const SCALA_WEB_DESC = "Full stack developer for the UMass College of Information and Computer Science's MS and " +
+    "PhD Admissions website. The system is used annually by over 80 faculty members of the CICS department to review over 2000 applicants per year. " +
+    "I was responsible for improving relational database design patterns by rewriting old PostgreSQL schemas, addressing plugin compatibility" +
+    " issues on the front end by migrating from Scala.js to Typescript + React, implementing a custom sorting algorithm to rank " +
+    "schools according to CS quality, and creating a cache to preserve user preferences between login sessions.";
+
+export const GOOGLE_INTERN_DESC = "Software Engineer Intern on the Site Reliability Team. I was responsible for creating a website " +
+    "encompassing the functionality of a command-line tool used by Google engineers to automate dependency resolution for their code release pipeline." +
+    " Authored a project design document detailing the problem statement, system requirements, UI mock-ups, user stories, test cases, " +
+    "design trade-offs and future development of the new web application. On the back end, I implemented an indexing pipeline in Go " +
+    "that broadcasts database updates to the webserver to keep the most recent results ready in cache.";
+
+export const CRA_INTERN_DESC = "Software Engineer Intern within the Decision Management division. Aided in full-stack development of a React.js " +
+    "application designed to detect system hardware failures on-board ships in the Navy. Improved fault detecting by ~15% by formulating Bayesian Network " +
+    " models using the Figaro probabilistic programming package for Scala. I used the new classification results to create a d3.js " +
+    "visualization to inform ship operators where faults are likely to happen.";
+
+export const UGTA_DESC = "Undergraduate Teaching Assistant for CS 220 (Functional Programming Using Scala). Worked as one of six " +
+    "undergraduates chosen to orchestrate class discussions sections and hold weekly office hours for a class of over 150 students. " +
+    "Introduced students to functional programming concepts such as immutability, higher-order functions, algebraic data types, recursive data structures " +
+    "and referential transparency.";
+
+export const TXTADVICE_DESC = "Led the development of an online SMS/MMS messaging service providing users with real-time fashion " +
+    "advice from professional stylists (featured on CNN!). I built a RESTful API using Flask + PostgreSQL, and implemented the messaging " +
+    "interface using React.js. A bit further down the line, I was able to write Postgres queries to analyse usage statistics to better " +
+    "inform stylists of the clothing preferences for a given user.";
--- www/react/resume.tsx
+++ www/react/resume.tsx
@@ -7,8 +7,22 @@ import {
 } from 'react-accessible-accordion';
 import 'react-accessible-accordion/dist/fancy-example.css';
 
+import {
+	CRA_INTERN_DESC,
+	GOOGLE_INTERN_DESC,
+	SCALA_WEB_DESC,
+	TXTADVICE_DESC,
+	UGTA_DESC
+} from "./descriptions/experience";
+
 interface ExperienceItemProps { timeFrame: string, company: string, location: string, jobTitle: string,
 	                            description: string, imageUrl?: string }
+
+interface EducationProps { timeFrame: string, school: string, location: string, major: string, description: string,
+	                       imageUrl?: string, gpa: string, courses: JSX.Element }
+
+interface AwardProps { timeFrame: string, event: string, location: string, award: string, description: string,
+	                   imageUrl?: string }
 
 interface ResumeProps {}
 interface ResumeState {}
@@ -33,11 +47,14 @@ export class Resume extends React.Component<ResumeProps, ResumeState> {
 				</div>
 				<Accordion>
 					<Experience />
+					<Education />
+					<Awards />
 				</Accordion>
 			</div>
 		)
 	}
 }
+
 
 /**
  * Functional component that renders the experience accordion section within the resume
@@ -51,19 +68,19 @@ const Experience = () => {
 				</AccordionItemTitle>
 				<AccordionItemBody>
 					<ExperienceItem timeFrame="Sep 2017 - Now" company="UMass Amherst" location="Amherst, MA"
-									jobTitle="Scala Web Developer" description="Scala web developer"
+									jobTitle="Scala Web Developer" description={SCALA_WEB_DESC}
 					                imageUrl="../static/img/umass-logo.png" />
 					<ExperienceItem timeFrame="May 2018 - Aug 2018" company="Google" location="Cambridge, MA"
-									jobTitle="Software Engineer Intern" description="SWE Intern, site reliability"
+									jobTitle="Software Engineer Intern" description={GOOGLE_INTERN_DESC}
 					                imageUrl="../static/img/google-logo.png" />
 					<ExperienceItem timeFrame="May 2017 - Aug 2017" company="Charles River Analytics" location="Cambridge, MA"
-									jobTitle="Software Engineer Intern" description="SWE Intern, Decision Management"
+									jobTitle="Software Engineer Intern" description={CRA_INTERN_DESC}
 					                imageUrl="../static/img/cra-logo.jpg" />
 					<ExperienceItem timeFrame="Sep 2016 - May 2018" company="UMass Amherst" location="Amherst, MA"
-									jobTitle="Undergraduate Teaching Assistant" description="UG TA for CS 220"
+									jobTitle="Undergraduate Teaching Assistant" description={UGTA_DESC}
 					                imageUrl="../static/img/umass-logo.png" />
 					<ExperienceItem timeFrame="May 2015 - Nov 2015" company="TxtAdvice" location="West Orange, NJ"
-									jobTitle="Lead Platform Developer" description="Lead platform dev for TxtAdvice"
+									jobTitle="Lead Platform Developer" description={TXTADVICE_DESC}
 					                imageUrl="../static/img/txtadvice-logo.png" />
 				</AccordionItemBody>
 			</AccordionItem>
@@ -88,8 +105,125 @@ const ExperienceItem = (props: ExperienceItemProps) => {
 				</div>
 				<div className="col-sm-8">
 					<h4 className="job-title">{props.jobTitle}</h4>
+					<p>{props.description}</p>
 				</div>
 			</div>
 		</div>
 	);
+};
+
+
+/**
+ * Functional component that renders the education accordion section within the resume
+ * @constructor
+ */
+const Education = () => {
+	const courses = (<div className="row">
+		                 <div className="col-sm-6">
+							 <ul className="course-list">
+								 <li>Software Engineering</li>
+								 <li>Digital Forensics</li>
+								 <li>Operating Systems</li>
+								 <li>Artificial Intelligence</li>
+								 <li>Natural Language Processing</li>
+								 <li>Machine Learning</li>
+							 </ul>
+						 </div>
+		                 <div className="col-sm-6">
+							 <ul className="course-list">
+								 <li>Ordinary Differential Equations</li>
+								 <li>Intro to Abstract Algebra</li>
+								 <li>Mathematics of Finance</li>
+								 <li>Applied Linear Algebra</li>
+								 <li>Intro to Scientific Computing</li>
+								 <li>Combinatorics &amp; Graph Theory</li>
+							 </ul>
+						 </div>
+	                 </div>);
+	return (
+        <AccordionItem>
+				<AccordionItemTitle>
+					<h3><i className=" fas fa-graduation-cap"></i>    Education</h3>
+				</AccordionItemTitle>
+				<AccordionItemBody>
+					<EducationItem timeFrame="Sep 2015 - May 2019" school="UMass Amherst" location="Amherst, MA"
+									major="Computer Science and Mathematics (BS)" description="umass amherst school"
+					                imageUrl="../static/img/umass-logo.png" courses={courses} gpa="3.93/4.0" />
+				</AccordionItemBody>
+		</AccordionItem>
+	);
+};
+
+/**
+ * Functional component that renders an education timeline item
+ * @param props
+ * @constructor
+ */
+const EducationItem = (props: EducationProps) => {
+    return (
+    	<div className="education-item">
+            <div className="row">
+				<div className="col-sm-4 edu-info">
+					<h4 className="edu-timeframe">{props.timeFrame}</h4>
+					<h4 className="edu-school">{props.school}</h4>
+					<p className="edu-gpa">GPA: {props.gpa}</p>
+					<p className="edu-location"><i className="fas fa-map-marker-alt"></i> {props.location}</p>
+					{ props.imageUrl &&
+						<img className="company-logo" src={props.imageUrl} alt="Company Logo"/>
+					}
+				</div>
+				<div className="col-sm-8">
+					<h4 className="job-title">{props.major}</h4>
+					{props.courses}
+				</div>
+			</div>
+		</div>
+	)
+};
+
+
+/**
+ * Functional component that renders the awards accordion section within the resume
+ * @constructor
+ */
+const Awards = () => {
+	return (
+        <AccordionItem>
+				<AccordionItemTitle>
+					<h3><i className="fas fa-medal"></i>    Awards</h3>
+				</AccordionItemTitle>
+				<AccordionItemBody>
+					<AwardItem timeFrame="Oct 2015" event="Hack UMass III" location="Amherst, MA" award="3rd Place"
+							   description="umass amherst school" imageUrl="../static/img/hackumass-logo.png" />
+					<AwardItem timeFrame="April 2015" event="NJ SkillsUSA State Championships" location="Bridgewater, NJ"
+							   award="2nd Place" description="Skills uSA" imageUrl="../static/img/skillsusa-logo.jpeg" />
+				</AccordionItemBody>
+		</AccordionItem>
+	);
+};
+
+/**
+ * Functional component that renders an award timeline item
+ * @param props
+ * @constructor
+ */
+const AwardItem = (props: AwardProps) => {
+    return (
+    	<div className="award-item">
+            <div className="row">
+				<div className="col-sm-4 award-info">
+					<h4 className="award-timeframe">{props.timeFrame}</h4>
+					<h4 className="award-title">{props.event}</h4>
+					<p className="award-location"><i className="fas fa-map-marker-alt"></i> {props.location}</p>
+					{ props.imageUrl &&
+						<img className="company-logo" src={props.imageUrl} alt="Event Logo"/>
+					}
+				</div>
+				<div className="col-sm-8">
+					<h4 className="job-title">{props.award}</h4>
+					<p>{props.description}</p>
+				</div>
+			</div>
+		</div>
+	)
 };
